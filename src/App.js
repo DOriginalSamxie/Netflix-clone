81d8d3e0e46bf754510ed18bf8618df687308d0f
DOriginalSamxie <samadrufai@gmail.com>
2022-11-03 03:56:55 +0100

    created the netflix logo,avatar and the banner

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './App.css'
+import Banner from './Banner'
+import HomeScreen from './HomeScreen'
+
+function App() {
+  return (
+    <div className='app'>
+      <HomeScreen />
+      <Banner />
+    </div>
+  )
+}
+
+export default App
---
+++ src/firebaseconfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDMFs4A-xDTvZCPLCr2JP8CZfuLv8xuY1s',
+  authDomain: 'netflix-clone-5fd26.firebaseapp.com',
+  projectId: 'netflix-clone-5fd26',
+  storageBucket: 'netflix-clone-5fd26.appspot.com',
+  messagingSenderId: '494802804925',
+  appId: '1:494802804925:web:b1ed82a3b31310d7a497d4',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
